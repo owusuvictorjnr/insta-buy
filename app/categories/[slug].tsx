f0d7808c5fb70d40d3313d2b606f4471479caa4d
owusuvictorjnr <owusuvictor80@gmail.com>
2024-12-19 15:55:36 +0000

    use stack.screen    and style item

--- app/categories/[slug].tsx
+++ app/categories/[slug].tsx
@@ -1,14 +1,79 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { Image, StyleSheet, Text, View } from "react-native";
+import React from "react";
+import { Redirect,  Stack,  useLocalSearchParams } from "expo-router";
+import { CATEGORIES } from "@/assets/category";
+import { PRODUCTS } from "@/assets/products";
 
 const Category = () => {
+  const { slug } = useLocalSearchParams<{ slug: string }>();
+
+  const category = CATEGORIES.find((category) => category.slug === slug);
+
+  if (!category) return <Redirect href="/404" />;
+
+  const products = PRODUCTS.filter((product) => product.category.slug === slug);
   return (
-    <View>
-      <Text>Category</Text>
+    <View style={styles.container}>
+      <Stack.Screen options={{ title: category.name }} />
+
+      <Image source={{uri: category.imageUrl}} style={styles.categoryImage} />
     </View>
-  )
-}
+  );
+};
 
-export default Category
+export default Category;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    padding: 16,
+  },
+
+  categoryImage: {
+    width: "100%",
+    height: 200,
+    marginBottom: 16,
+    resizeMode: "cover",
+    borderRadius: 8,
+  },
+
+  categoryName: {
+    fontSize: 24,
+    fontWeight: "bold",
+    marginBottom: 16,
+  },
+
+  productsList: {
+    flexGrow: 1,
+  },
+
+  productRow: {
+    justifyContent: "space-between",
+  },
+
+  productContainer: {
+    flex: 1,
+    margin: 8,
+  },
+
+  productImage: {
+    width: "100%",
+    height: 200,
+    resizeMode: "cover",
+    borderRadius: 8,
+  },
+
+  productTitle: {
+    fontSize: 16,
+    fontWeight: "bold",
+    marginBottom: 8,
+  },
+
+  productPrice: {
+    fontSize: 16,
+    fontWeight: "bold",
+    color: "#888",
+    marginTop: 4,
+  },
+});
